95e60b179630cdbafbde6d5fd043e9226c2fd444
james <guellati@gmail.com>
2019-03-29 17:43:24 +0100

    Add gradient effect on image

--- reciplease/Extensions/IBInspectableExtension.swift
+++ reciplease/Extensions/IBInspectableExtension.swift
@@ -36,38 +36,7 @@ extension UIView {
             layer.borderColor = newValue.cgColor
         }
     }
-    @IBInspectable var shadowColor: UIColor {
-        get {
-            return UIColor(cgColor: layer.shadowColor!)
-        }
-        set {
-            layer.shadowColor = newValue.cgColor
-        }
-    }
-    @IBInspectable var shadowOpacity: Float {
-        get {
-            return layer.shadowOpacity
-        }
-        set {
-            layer.shadowOpacity = newValue
-        }
-    }
-    @IBInspectable var shadowOffset: CGSize {
-        get {
-            return layer.shadowOffset
-        }
-        set {
-            layer.shadowOffset = newValue
-        }
-    }
-    @IBInspectable var shadowRadius: CGFloat {
-        get {
-            return layer.shadowRadius
-        }
-        set {
-            layer.shadowRadius = newValue
-        }
-    }
+  
     @IBInspectable var layerMasksToBounds: Bool {
         get {
             return layer.masksToBounds
---
+++ reciplease/Extensions/SHGradientView.swift
@@ -0,0 +1,52 @@
+//
+//  SHGradientView.swift
+//  SwiftHelpers
+//
+//  Created by intergoldex llc  on 04/07/2016.
+//  Copyright © 2016 intergoldex. All rights reserved.
+//
+
+import UIKit
+
+/**
+ A class that allows to easily add gradients (and from Interface Builder too)
+ It will create a gradient from the view's background colour to a clear colour
+ */
+@IBDesignable
+class GradientView: UIView {
+    
+    @IBInspectable var startColor:   UIColor = .black { didSet { updateColors() }}
+    @IBInspectable var endColor:     UIColor = .white { didSet { updateColors() }}
+    @IBInspectable var startLocation: Double =   0.05 { didSet { updateLocations() }}
+    @IBInspectable var endLocation:   Double =   0.95 { didSet { updateLocations() }}
+    @IBInspectable var horizontalMode:  Bool =  false { didSet { updatePoints() }}
+    @IBInspectable var diagonalMode:    Bool =  false { didSet { updatePoints() }}
+    
+    override class var layerClass: AnyClass { return CAGradientLayer.self }
+    
+    var gradientLayer: CAGradientLayer { return layer as! CAGradientLayer }
+    
+    func updatePoints() {
+        if horizontalMode {
+            gradientLayer.startPoint = diagonalMode ? CGPoint(x: 1, y: 0) : CGPoint(x: 0, y: 0.5)
+            gradientLayer.endPoint   = diagonalMode ? CGPoint(x: 0, y: 1) : CGPoint(x: 1, y: 0.5)
+        } else {
+            gradientLayer.startPoint = diagonalMode ? CGPoint(x: 0, y: 0) : CGPoint(x: 0.5, y: 0)
+            gradientLayer.endPoint   = diagonalMode ? CGPoint(x: 1, y: 1) : CGPoint(x: 0.5, y: 1)
+        }
+    }
+    func updateLocations() {
+        gradientLayer.locations = [startLocation as NSNumber, endLocation as NSNumber]
+    }
+    func updateColors() {
+        gradientLayer.colors    = [startColor.cgColor, endColor.cgColor]
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        updatePoints()
+        updateLocations()
+        updateColors()
+    }
+}
+
